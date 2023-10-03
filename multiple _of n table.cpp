973368294ea70bcfa64d512979ffc5388eaecebd
DebjyotiSayan <143388775+DebjyotiSayan@users.noreply.github.com>
2023-10-03 16:48:59 +0530

    Add files via upload

---
+++ multiple _of n table.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+     int n;
+	 printf("enter the value of n");
+	 scanf("%d" ,&n);
+	 for(int i=n;i<=10*n;i=i+n){
+	 
+	printf("%d ",i);
+	 }
+	 
+    return 0;
+
+}
