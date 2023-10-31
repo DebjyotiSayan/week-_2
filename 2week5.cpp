2728eacb5e51dc912bfba64c3af5e875d3668c0b
DebjyotiSayan <143388775+DebjyotiSayan@users.noreply.github.com>
2023-10-31 16:32:28 +0530

    Add files via upload

---
+++ 2week5.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int temp,n,i,j;
+	printf("enter n");
+	scanf("%d",&n);
+	printf("enter the array element");
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+	scanf("%d", &a[i]);
+	}
+	for(int i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if(a[j]<a[j+1]){
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	printf("the shorted array are ");
+	for(int i=n;i<0;i--){
+		printf("%d ",a[i]);
+}
+	return 0;
+}
